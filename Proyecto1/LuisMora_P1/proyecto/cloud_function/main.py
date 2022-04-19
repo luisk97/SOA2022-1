d5faabc90d5aff5fa09f020e27d2096fecf68a1a
luisk97 <30986170+luisk97@users.noreply.github.com>
2022-04-18 23:30:01 -0600

    Solución error validación datos de entrada

--- Proyecto1/LuisMora_P1/proyecto/cloud_function/main.py
+++ Proyecto1/LuisMora_P1/proyecto/cloud_function/main.py
@@ -12,37 +12,40 @@ vision_client = vision.ImageAnnotatorClient()
 def main(data, context):# pylint: disable=missing-function-docstring
     print("Evento-contexto", context)
     
-    print(f"A ver {data}.")
-
-    file_data = data
-
-    file_name = file_data["name"]
+    print(f"Data: {data}.")
     
-    bucket_name = file_data["bucket"]
+    if (data == {}):
+        print("Sin datos de entrada")
+    else:
+        file_data = data
 
-    #blob = storage_client.bucket(bucket_name).get_blob(file_name)
-    blob_uri = f"gs://{bucket_name}/{file_name}"
+        file_name = file_data["name"]
 
-    blob_source = vision.Image(source=vision.ImageSource(image_uri=blob_uri))
+        bucket_name = file_data["bucket"]
+
+        #blob = storage_client.bucket(bucket_name).get_blob(file_name)
+        blob_uri = f"gs://{bucket_name}/{file_name}"
+
+        blob_source = vision.Image(source=vision.ImageSource(image_uri=blob_uri))
 
 
-    print(f"Analyzing {file_name}.")
+        print(f"Analyzing {file_name}.")
 
-    """ image_to_open = 'images/cara.jpg'
+        """ image_to_open = 'images/cara.jpg'
 
-    with open(image_to_open, 'rb') as image_file:
-        content = image_file.read()
+        with open(image_to_open, 'rb') as image_file:
+            content = image_file.read()
 
-    image = vision.Image(content=content) """
+        image = vision.Image(content=content) """
 
-    face_response = vision_client.face_detection(image=blob_source)
+        face_response = vision_client.face_detection(image=blob_source)
 
-    for face_detection in face_response.face_annotations:
-        d={
-            'confidence': face_detection.detection_confidence,
-            'joy': face_detection.joy_likelihood,
-            'sorrow': face_detection.sorrow_likelihood,
-            'surprise': face_detection.surprise_likelihood,
-            'anger': face_detection.anger_likelihood
-        }
-        print(d)
+        for face_detection in face_response.face_annotations:
+            d={
+                'confidence': face_detection.detection_confidence,
+                'joy': face_detection.joy_likelihood,
+                'sorrow': face_detection.sorrow_likelihood,
+                'surprise': face_detection.surprise_likelihood,
+                'anger': face_detection.anger_likelihood
+            }
+            print(d)
